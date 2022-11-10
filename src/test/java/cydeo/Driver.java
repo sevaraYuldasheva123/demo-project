fb19d6f7b1d4abd34a4604eb529939172bc7d60c
Sevara Yuldasheva <sevara878787@mail.ru>
2022-11-10 15:15:28 -0500

    new updates

--- src/test/java/cydeo/Driver.java
+++ src/test/java/cydeo/Driver.java
@@ -35,7 +35,7 @@ public class Driver {
                 case "remote-chrome":
                     try {
                         // assign your grid server address
-                        String gridAddress = "34.237.243.216";
+                        String gridAddress = "34.228.160.113";
                         URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
                         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
                         desiredCapabilities.setBrowserName("chrome");
